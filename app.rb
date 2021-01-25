18a5005f6efc75a450fa31c4e1365c6f97a5caa7
parisdolfman <parisdolfman@gmail.com>
2021-01-25 12:57:20 -0500

    Done.

--- app.rb
+++ app.rb
@@ -5,5 +5,23 @@ module FormsLab
 
     # code other routes/actions here
 
+    get "/" do
+      erb :new
+    end 
+
+    post "/pirates" do
+
+      @pirate = Pirate.new(params[:pirate])
+
+      params[:name][:weight][:height].each do |details|
+        Pirate.new(details)
+      end 
+
+      @pirates = Pirate.all
+
+      erb :show
+
+    end 
+
   end
 end
